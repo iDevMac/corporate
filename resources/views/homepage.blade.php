57335fbc833e26fc5064627583880c72fa72accc
Prince Bolarin <idevmac.ops@gmail.com>
2024-11-13 10:59:15 +0100

    first commit

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,44 @@
+@extends("front.layouts.layout")
+@section("content")
+     <!-- Clients Section -->
+
+    <!-- /Clients Section -->
+
+    <!-- About Section -->
+    @include("front.about")
+    <!-- /About Section -->
+
+    <!-- Stats Section -->
+    
+    <!-- /Stats Section -->
+
+    <!-- Why Us Section -->
+    
+    <!-- /Why Us Section -->
+
+    <!-- Call To Action Section -->
+    @include("front.call-to-action")
+    <!-- /Call To Action Section -->
+
+    <!-- Services Section -->
+    @include("front.services")
+    <!-- /Services Section -->
+
+    <!-- Portfolio Section -->
+    
+    <!-- /Portfolio Section -->
+
+    <!-- Testimonials Section -->
+    
+    <!-- /Testimonials Section -->
+
+    <!-- Team Section -->
+    
+    <!-- /Team Section -->
+
+    <!-- Contact Section -->
+    @include("front.contact")
+    <!-- /Contact Section -->
+
+@endsection
+
